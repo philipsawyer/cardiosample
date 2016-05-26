92e46dc14f1cddb15f9dda6212ee0ff560bc8118
Philip Sawyer <philsawyer94@gmail.cm>
2016-05-26 16:47:55 -0400

    Added new view controllers
    added view controllers for the card subviews and the actuall viewcontroller

--- CardIOSample/CardIOSample/DisplaySVViewController.swift
+++ CardIOSample/CardIOSample/DisplaySVViewController.swift
@@ -8,10 +8,53 @@
 
 import UIKit
 
-class DisplaySVViewController: UIViewController {
+class DisplaySVViewController: UIViewController, CardIOViewDelegate {
+    
+    @IBOutlet weak var scanCardButton: UIButton!
+    @IBOutlet weak var resultsLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        if !CardIOUtilities.canReadCardWithCamera() {
+            self.scanCardButton.hidden = true
+            self.resultsLabel.text = "Camera is not supported"
+        }
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
         
+        CardIOUtilities.preload()
+    }
+    
+    func cardIOView(cardIOView: CardIOView!, didScanCard cardInfo: CardIOCreditCardInfo!) {
+        print("didScanCard")
+        
+        if cardInfo != nil {
+            let result = NSString(format: "Received card info.\n Number: %@\n expiry: %02lu/%lu\n cvv: %@.", cardInfo.redactedCardNumber, cardInfo.expiryMonth, cardInfo.expiryYear, cardInfo.cvv)
+            self.resultsLabel.text = result as String
+        } else {
+            self.resultsLabel.text = "Error occured in scanning card"
+        }
+        
+        cardIOView.removeFromSuperview()
+    }
+    
+    @IBAction func scanCard(sender: AnyObject) {
+        let rect = CGRect(x: 10, y: 10, width: 200, height: 200)
+        let cardIOView = CardIOView(frame: rect)
+        cardIOView.delegate = self
+        
+        self.view.addSubview(cardIOView)
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        for view in self.view.subviews {
+            if view.isKindOfClass(CardIOView) {
+                view.removeFromSuperview()
+            }
+        }
     }
 }

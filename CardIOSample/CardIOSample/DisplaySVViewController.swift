507d45964043d9568056cf7945634a2214ec003c
Philip Sawyer <philsawyer94@gmail.cm>
2016-05-26 15:59:27 -0400

    added tab bar

---
+++ CardIOSample/CardIOSample/DisplaySVViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DisplaySVViewController.swift
+//  CardIOSample
+//
+//  Created by Philip Sawyer on 5/26/16.
+//  Copyright © 2016 Philip Sawyer. All rights reserved.
+//
+
+import UIKit
+
+class DisplaySVViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
--- CardIOSample/CardIOSample/DisplayVCViewController.swift
+++ CardIOSample/CardIOSample/DisplayVCViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, CardIOPaymentViewControllerDelegate {
+class DisplayVCViewController: UIViewController, CardIOPaymentViewControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()

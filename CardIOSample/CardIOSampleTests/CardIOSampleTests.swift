66b3eee4718ff233b9f37cee8c46e616625098d8
Philip Sawyer <philsawyer94@gmail.cm>
2016-05-26 09:30:05 -0400

    initial commit

---
+++ CardIOSample/CardIOSampleTests/CardIOSampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  CardIOSampleTests.swift
+//  CardIOSampleTests
+//
+//  Created by Philip Sawyer on 5/26/16.
+//  Copyright © 2016 Philip Sawyer. All rights reserved.
+//
+
+import XCTest
+@testable import CardIOSample
+
+class CardIOSampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
